e7f25571cc04c11aa4cb32e70a5fd97ed6be95b7
MandirHuasco <mhuasco011@gmail.com>
2021-12-20 00:59:42 -0500

    Mandir
    Almacen v1.2

--- src/Principal.js
+++ src/Principal.js
@@ -1,9 +1,102 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import img101 from './img/logo-empresa-almacen.png';
 
 function Principal() {
+
+    const [styleActiveA, setStyleActiveA] = useState("");
+    const [styleActiveB, setStyleActiveB] = useState("");
+    const [styleActiveC, setStyleActiveC] = useState("");
+    const [styleActiveD, setStyleActiveD] = useState("");
+    const [styleActiveE, setStyleActiveE] = useState("");
+    const [styleActiveF, setStyleActiveF] = useState("");
+    const [styleActiveG, setStyleActiveG] = useState("");
+
+
+    const changeStyleActiveA = () => {
+        console.log("menu-action-Active-A");
+
+        setStyleActiveA("active");
+        setStyleActiveB("");
+        setStyleActiveC("");
+        setStyleActiveD("");
+        setStyleActiveE("");
+        setStyleActiveF("");
+        setStyleActiveG("");
+    };
+
+    const changeStyleActiveB = () => {
+        console.log("menu-action-Active-B");
+
+        setStyleActiveA("");
+        setStyleActiveB("active");
+        setStyleActiveC("");
+        setStyleActiveD("");
+        setStyleActiveE("");
+        setStyleActiveF("");
+        setStyleActiveG("");
+    };
+
+    const changeStyleActiveC = () => {
+        console.log("menu-action-Active-C");
+
+        setStyleActiveA("");
+        setStyleActiveB("");
+        setStyleActiveC("active");
+        setStyleActiveD("");
+        setStyleActiveE("");
+        setStyleActiveF("");
+        setStyleActiveG("");
+    };
+
+    const changeStyleActiveD = () => {
+        console.log("menu-action-Active-D");
+
+        setStyleActiveA("");
+        setStyleActiveB("");
+        setStyleActiveC("");
+        setStyleActiveD("active");
+        setStyleActiveE("");
+        setStyleActiveF("");
+        setStyleActiveG("");
+    };
+
+    const changeStyleActiveE = () => {
+        console.log("menu-action-Active-E");
+
+        setStyleActiveA("");
+        setStyleActiveB("");
+        setStyleActiveC("");
+        setStyleActiveD("");
+        setStyleActiveE("active");
+        setStyleActiveF("");
+        setStyleActiveG("");
+    };
+
+    const changeStyleActiveF = () => {
+        console.log("menu-action-Active-F");
+
+        setStyleActiveA("");
+        setStyleActiveB("");
+        setStyleActiveC("");
+        setStyleActiveD("");
+        setStyleActiveE("");
+        setStyleActiveF("active");
+        setStyleActiveG("");
+    };
+
+    const changeStyleActiveG = () => {
+        console.log("menu-action-Active-G");
+
+        setStyleActiveA("");
+        setStyleActiveB("");
+        setStyleActiveC("");
+        setStyleActiveD("");
+        setStyleActiveE("");
+        setStyleActiveF("");
+        setStyleActiveG("active");
+    };
 
     return (
 
@@ -12,44 +105,44 @@ function Principal() {
                 <div className="logo-nav">
                     <img src={img101} className="img-logo-nav"/>
                 </div>
-                <li className="list active list-pad">
-                    <a href="#">
+                <li className={styleActiveA + " list list-pad"}>
+                    <a href="#" onClick={changeStyleActiveA}>
                         <span className="icon"><ion-icon name="cube-outline"></ion-icon></span>
                         <span className="title">Productos</span>
                     </a>
                 </li>
-                <li className="list">
-                    <a href="#">
+                <li className={styleActiveB + " list"}>
+                    <a href="#" onClick={changeStyleActiveB}>
                         <span className="icon"><ion-icon name="grid-outline"></ion-icon></span>
                         <span className="title">Almacen</span>
                     </a>
                 </li>
-                <li className="list">
-                    <a href="#">
+                <li className={styleActiveC + " list"}>
+                    <a href="#" onClick={changeStyleActiveC}>
                         <span className="icon"><ion-icon name="newspaper-outline"></ion-icon></span>
                         <span className="title">Reportes</span>
                     </a>
                 </li>
-                <li className="list">
-                    <a href="#">
+                <li className={styleActiveD + " list"}>
+                    <a href="#" onClick={changeStyleActiveD}>
                         <span className="icon"><ion-icon name="people-circle-outline"></ion-icon></span>
                         <span className="title">Usuarios</span>
                     </a>
                 </li>
-                <li className="list">
-                    <a href="#">
+                <li className={styleActiveE + " list"}>
+                    <a href="#" onClick={changeStyleActiveE}>
                         <span className="icon"><ion-icon name="settings-outline"></ion-icon></span>
                         <span className="title">Configuración</span>
                     </a>
                 </li>
-                <li className="list">
-                    <a href="#">
+                <li className={styleActiveF + " list"}>
+                    <a href="#" onClick={changeStyleActiveF}>
                         <span className="icon"><ion-icon name="help-outline"></ion-icon></span>
                         <span className="title">Ayuda</span>
                     </a>
                 </li>
-                <li className="list Sign-off">
-                    <a href="#">
+                <li className={styleActiveG + " list Sign-off"}>
+                    <a href="#" onClick={changeStyleActiveG}>
                         <span className="icon"><ion-icon name="log-out-outline"></ion-icon></span>
                         <span className="title">Salir</span>
                     </a>
@@ -58,12 +151,6 @@ function Principal() {
         </div>
     );
 }
-const list = document.querySelectorAll('.list');
-function activeLink(){
-    list.forEach((item) => item.classList.remove('active'));
-    this.classList.add('active');
-}
-list.forEach((item) => item.addEventListener('click',activeLink));
 
 
 export default Principal;
